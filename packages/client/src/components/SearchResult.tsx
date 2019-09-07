976b105009c926b783c1a658a86fc35417f0cdde
gillchristian <gillchristiang@gmail.com>
2019-09-07 12:38:17 +0200

    fix(client): Use new stringify

--- packages/client/src/components/SearchResult.tsx
+++ packages/client/src/components/SearchResult.tsx
@@ -6,19 +6,13 @@ import {
   gruvboxDark,
 } from 'react-syntax-highlighter/dist/styles/hljs'
 
-import { Param, FunctionRecord } from 'ts-earch-types'
+import { FunctionRecord, stringifySignature } from 'ts-earch-types'
 
 import Collapse from './Collapse'
 
 interface Props {
   result: FunctionRecord
 }
-
-// https://github.com/conorhastings/react-syntax-highlighter
-const signature = (parameters: Param[], returnType: string, fnName?: string) =>
-  `${fnName || 'func'} :: (${parameters
-    .map(({ name, type }) => `${name}: ${type}`)
-    .join(', ')}) => ${returnType}`
 
 const Container = styled.div({
   marginBottom: 30,
@@ -44,17 +38,13 @@ const signatureStyle = {
   marginBottom: 10,
 }
 
-const Signature: React.SFC<FunctionRecord> = ({
-  name,
-  parameters,
-  returnType,
-}) => (
+const Signature: React.SFC<FunctionRecord> = props => (
   <SyntaxHighlighter
     language="typescript"
     style={gruvboxLight}
     customStyle={signatureStyle}
   >
-    {signature(parameters, returnType, name)}
+    {stringifySignature(props.signature)}
   </SyntaxHighlighter>
 )
 
